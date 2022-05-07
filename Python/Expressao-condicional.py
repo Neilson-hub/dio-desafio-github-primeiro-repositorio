d5d309cbd9316d5c5ded7e68a5fa7352107cec0c
Perkles <neilsonfseverino@gmail.com>
2022-05-07 06:22:36 -0400

    Inclusão das anotações do curso de Git/GitHub

---
+++ Python/Expressao-condicional.py
@@ -0,0 +1,30 @@
+"""
+expressão condicioanal com operador  OR
+"""
+# ------------- Exemplo-4 ---------------------------------
+a = 0 
+b = None
+c = False
+d = []
+e = {}
+f = 22
+g = 'Luiz'
+
+variavel = a or b or c or d or e or f or g
+print(variavel)
+# ------------- Exemplo-3 ------------------------
+#nome = input('Qual o seu nome? ')
+#print(nome or None or False or 0 or 'Você não digitou nada.' or 'Outra coisa')
+# ------------- Exemplo-2 -------------------------
+#nome = input('Qual o seu nome? ')
+#print(nome or 'Você não digitou nada!')
+
+
+# ------------- Exemplo-1 --------------------------
+
+#nome = input('Qual o seu nome? ')
+
+#if nome:
+   # print(nome)
+#else:
+#    print('Você não digitou nada =(')
